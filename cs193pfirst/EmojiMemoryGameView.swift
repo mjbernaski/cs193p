f31d96e9ed47e08d11f63ef5e4d4492e1ab8ea79
Michael Bernaski <39862404+mjbernaski@users.noreply.github.com>
2024-02-13 17:06:22 -0600

    added geometry readery

--- cs193pfirst/EmojiMemoryGameView.swift
+++ cs193pfirst/EmojiMemoryGameView.swift
@@ -16,35 +16,69 @@ struct EmojiMemoryGameView: View  {
     
     var body: some View {
         VStack {
-            ScrollView {
                 cards
                     .animation(.default, value: viewModel.cards)
-            }
             Spacer()
             Button("Shuffle") {
                 viewModel.shuffle()
             }
-        }
+        } 
+       
     }
     
     var cards: some View {
-        
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 65), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) {card in
-                CardView(card: card)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)  // Intents
-                    }
+        GeometryReader {geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: viewModel.cards.count,
+                size: geometry.size,
+                atAspectRatio: 2/3)
+            
+             LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)],spacing: 0) {
+                ForEach(viewModel.cards) {card in
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)  // Intents
+                        }
+                }
+
             }
-        }
-        .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-        .padding()
-        
+        } .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
     }
     
-
+    func gridItemWidthThatFits(
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat) -> CGFloat
+    {
+        print("size", size)
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        repeat {
+            let width = size.width / columnCount
+            print("columnCount ", columnCount)
+            print("count ", count)
+            print("width ", width)
+            print("size ", size)
+            let height = width / aspectRatio
+            print("height ", height)
+            print("size.height", size.height)
+            let rowCount = (count / columnCount)
+            print("rowCount ", rowCount)
+            print("rowCount * height", rowCount * height)
+            if (rowCount * height < size.height) {
+                print((size.width / columnCount).rounded(.down))
+                return (size.width / columnCount).rounded(.down)
+            }
+            columnCount += 1.0
+            print("--------------------")
+        } while (columnCount < count)
+        
+        print(min(size.width / count, size.height * aspectRatio).rounded(.down))
+        
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
+    }
 }
 
 struct CardView: View {
