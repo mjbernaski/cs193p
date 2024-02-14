b56da9f1a6bbed3f7f8e68246609042bb9a3ad67
Michael Bernaski <39862404+mjbernaski@users.noreply.github.com>
2024-02-14 07:47:49 -0600

    AspectVGrid works

---
+++ cs193pfirst/AspectVGrid.swift
@@ -0,0 +1,51 @@
+//
+//  AspectVGrid.swift
+//  cs193pfirst
+//
+//  Created by Michael Bernaski on 2/14/24.
+//
+
+import SwiftUI
+
+struct AspectVGrid<Item: Identifiable, ItemView: View >: View {
+    var items:[Item]
+    var aspectRatio: CGFloat = 1
+    var content: (Item) -> ItemView
+    var body: some View {
+        GeometryReader {geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: items.count,
+                size: geometry.size,
+                atAspectRatio: aspectRatio)
+            
+             LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)],spacing: 0) {
+                 ForEach(items) {item  in
+                     content(item).aspectRatio(2/3, contentMode: .fit)
+                 }
+    }
+}
+}
+    func gridItemWidthThatFits(
+                        count: Int,
+                        size: CGSize,
+                        atAspectRatio aspectRatio: CGFloat) -> CGFloat
+                    {
+                        let count = CGFloat(count)
+                        var columnCount = 1.0
+                        repeat {
+                            let width = size.width / columnCount
+                            let height = width / aspectRatio
+                            let rowCount = (count / columnCount)
+                            if (rowCount * height < size.height) {
+                                return (size.width / columnCount).rounded(.down)
+                            }
+                            columnCount += 1.0
+                        } while (columnCount < count)
+                        
+                        return min(size.width / count, size.height * aspectRatio).rounded(.down)
+                    }
+                    }
+
+// could do as a Layout
+
+
--- cs193pfirst/EmojiMemoryGameView.swift
+++ cs193pfirst/EmojiMemoryGameView.swift
@@ -30,16 +30,8 @@ struct EmojiMemoryGameView: View  {
     
     private var cards: some View {
         
-        GeometryReader {geometry in
-            let gridItemSize = gridItemWidthThatFits(
-                count: viewModel.cards.count,
-                size: geometry.size,
-                atAspectRatio: aspectRatio)
-            
-             LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)],spacing: 0) {
-                ForEach(viewModel.cards) {card in
+        AspectVGrid(items: viewModel.cards, aspectRatio: aspectRatio) {card in
                     CardView(card: card)
-                        .aspectRatio(2/3, contentMode: .fit)
                         .padding(4)
                         .onTapGesture {
                             viewModel.choose(card)  // Intents
@@ -48,28 +40,7 @@ struct EmojiMemoryGameView: View  {
 
             }
         }
-    }
     
-    func gridItemWidthThatFits(
-        count: Int,
-        size: CGSize,
-        atAspectRatio aspectRatio: CGFloat) -> CGFloat
-    {
-        let count = CGFloat(count)
-        var columnCount = 1.0
-        repeat {
-            let width = size.width / columnCount
-            let height = width / aspectRatio
-            let rowCount = (count / columnCount)
-            if (rowCount * height < size.height) {
-                return (size.width / columnCount).rounded(.down)
-            }
-            columnCount += 1.0
-        } while (columnCount < count)
-        
-        return min(size.width / count, size.height * aspectRatio).rounded(.down)
-    }
-}
 
 struct CardView: View {
     
